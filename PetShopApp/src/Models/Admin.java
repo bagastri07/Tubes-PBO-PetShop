9b9edc8512cbdc625e880835263595518bbb85c4
Weera17 <perwirahanifz@student.telkomuniversity.ac.id>
2021-12-23 22:53:32 +0700

    Membuat kelas admin, login admin, pemesanan, dan interface metode pembayaran

--- PetShopApp/src/Models/Admin.java
+++ PetShopApp/src/Models/Admin.java
@@ -10,5 +10,37 @@ package Models;
  * @author bagas
  */
 public class Admin {
+    private String idAdmin, nama, telepon;
+
+    public Admin(String idAdmin, String nama, String telepon) {
+        this.idAdmin = idAdmin;
+        this.nama = nama;
+        this.telepon = telepon;
+    }
+
+    public String getIdAdmin() {
+        return idAdmin;
+    }
+
+    public void setIdAdmin(String idAdmin) {
+        this.idAdmin = idAdmin;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getTelepon() {
+        return telepon;
+    }
+
+    public void setTelepon(String telepon) {
+        this.telepon = telepon;
+    }
+    
     
 }
