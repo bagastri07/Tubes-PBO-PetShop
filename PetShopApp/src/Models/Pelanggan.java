e91293d99ecf89c2416d7d182e92f25eef1a66af
bagastri07 <bagaskuhaku@gmail.com>
2021-12-23 23:01:39 +0700

    menambah pelanggan dan grooming

---
+++ PetShopApp/src/Models/Pelanggan.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Models;
+
+/**
+ *
+ * @author bagas
+ */
+public class Pelanggan {
+    private int id;
+    private String nama, nomorTelepon, alamat;
+
+    public Pelanggan(int id, String nama, String nomorTelepon, String alamat) {
+        this.id = id;
+        this.nama = nama;
+        this.nomorTelepon = nomorTelepon;
+        this.alamat = alamat;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public void setNomorTelepon(String nomorTelepon) {
+        this.nomorTelepon = nomorTelepon;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public String getNomorTelepon() {
+        return nomorTelepon;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+}
