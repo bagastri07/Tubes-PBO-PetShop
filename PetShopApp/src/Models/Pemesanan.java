bf157462a7db0c24e2c0401ed690b3fecefe36d8
Weera17 <perwirahanifz@student.telkomuniversity.ac.id>
2021-12-23 23:07:23 +0700

    update pemesanan

--- PetShopApp/src/Models/Pemesanan.java
+++ PetShopApp/src/Models/Pemesanan.java
@@ -9,6 +9,39 @@ package Models;
  *
  * @author Yoga
  */
-public class Pemesanan {
+public class Pemesanan implements MetodePembayaran{
+    private String nama;
+    private Pelanggan pelanggan;
+
+    public Pemesanan(String nama, Pelanggan pelanggan) {
+        this.nama = nama;
+        this.pelanggan = pelanggan;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public Pelanggan getPelanggan() {
+        return pelanggan;
+    }
+
+    public void setPelanggan(Pelanggan pelanggan) {
+        this.pelanggan = pelanggan;
+    }
+    
+    @Override
+    public void bayarDgnGopay() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    @Override
+    public void bayarDgnTunai() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
     
 }
