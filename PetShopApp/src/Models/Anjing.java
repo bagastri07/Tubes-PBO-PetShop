1893bcf53acc59829b1e5c39d19a6454ba54c994
Farrel Ceskicevas <farrelceskicevas@gmail.com>
2021-12-23 22:59:14 +0700

    Menambahkan Class Kucing dan Anjing

---
+++ PetShopApp/src/Models/Anjing.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Models;
+
+/**
+ *
+ * @author Farrel
+ */
+public class Anjing extends HewanPeliharaan{
+    public Anjing(String nama, String jenisKelamin, int Umur) {
+        super(nama, jenisKelamin, Umur);
+    }
+}
---
+++ PetShopApp/src/Models/Kucing.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Models;
+
+/**
+ *
+ * @author Farrel
+ */
+public class Kucing extends HewanPeliharaan{
+    public Kucing(String nama, String jenisKelamin, int Umur) {
+        super(nama, jenisKelamin, Umur);
+    }
+}
